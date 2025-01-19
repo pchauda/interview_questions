47dc4ea86fa9531cb1e53e341dc9f3fc634727ed
Prince <prince.chauda@gmail.com>
2025-01-19 08:09:10 -0500

    Initial commit

--- src/main/java/com/pc/interview/questions/graphs/ConnectedComponentsInUndirectedGraph.java
+++ src/main/java/com/pc/interview/questions/graphs/ConnectedComponentsInUndirectedGraph.java
@@ -15,11 +15,14 @@ import java.util.List;
 public class ConnectedComponentsInUndirectedGraph {
     public static void main(String[] args) {
         Graph g = new Graph(9);
+        // 0 -> 1 -> 2 -> 3
         g.addEdge(0, 1);
         g.addEdge(1, 2);
         g.addEdge(2, 3);
+        // 4 -> 5 -> 6
         g.addEdge(4, 5);
         g.addEdge(5, 6);
+        // 7 -> 8
         g.addEdge(7, 8);
 
         System.out.println("Connected components count: " + g.connectedComponents()); // Output: 3
